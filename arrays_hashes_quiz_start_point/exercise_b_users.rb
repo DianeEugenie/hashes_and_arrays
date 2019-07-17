df19d4470782305bd413b3cc163b34b5f229428b
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-07-17 16:52:36 +0100

    check if values in array are even and return even values in array

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -64,10 +64,19 @@ users["Erik"][:lottery_numbers]
 users["Avril"][:pets][0][:species]
 # 5. Get the smallest of Erik's lottery numbers
 users["Erik"][:lottery_numbers].min
-
-
-
 # 6. Return an array of Avril's lottery numbers that are even
+Avril_numbers = users["Avril"][:lottery_numbers]
+
+even_numbers = []
+
+for number in Avril_numbers
+  if (number.even?)
+    even_numbers.push(number)
+  end
+end
+
+p even_numbers
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
