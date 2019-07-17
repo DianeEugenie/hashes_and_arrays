4f8c76664ae0f57a18b758d431d5b3fc032f53f3
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-07-17 16:58:37 +0100

    change value inside a hash inside a hash

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -75,11 +75,13 @@ for number in Avril_numbers
   end
 end
 
-# return even_numbers
+return even_numbers
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 users["Erik"][:lottery_numbers].push(7)
 
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = "Edinburgh"
+
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users hash
